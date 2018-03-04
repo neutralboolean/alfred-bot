323468b32598b0f861225c7d9aebba5b5f85a111
Samuel O. Ugowe <sdougowe@gmail.com>
2018-03-03 21:24:03 -0500

    Update config.go

--- config.go
+++ config.go
@@ -3,9 +3,9 @@ package main
 
 //import "time"
 
-const Token string = "NDA5MDYzMDY0MzY1ODI2MDQ4.DVgfDw.ygF412spXYqPmF_UYk8jvZQgIX4"
-const Master string = "110575622635421696"		//set this up with the ID of the bot handler in your server
-												//mostly for fun features
+const Token string = "NDA5MDYzMDY0MzY1ODI2MDQ4.DVgfDw.ygF412spXYqPmF_UYk8jvZQgIX4" //bot's token
+const Master string = ""		//set this up with the ID of the bot handler in your server
+				        //mostly for fun features
 
 //"time"-based consts for Alfred's greetings
 //const MyLoc Location = time.LoadLocation("America/New_York")
