e417650229f726991f9ab4cd7f9763013fc8b886
Samuel O. Ugowe <sdougowe@gmail.com>
2018-03-19 20:30:09 -0400

    Update config.go

--- config.go
+++ config.go
@@ -3,7 +3,7 @@ package main
 
 //import "time"
 
-const Token string = "NDA5MDYzMDY0MzY1ODI2MDQ4.DVgfDw.ygF412spXYqPmF_UYk8jvZQgIX4" //bot's token
+const Token string = ""//bot's token
 const Master string = ""		//set this up with the ID of the bot handler in your server
 				        //mostly for fun features
 
